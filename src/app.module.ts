f06c5a0c6aca468a46c5deda0c7b25a9cc32b8e3
Stiven <stivenpilca@gmail.com>
2025-03-06 19:55:39 -0500

    refactor(app)/docs(app): improve code & add inline comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,22 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { LogsModule } from './logs/logs.module';
+import { PaymentModule } from './payment/payment.module';
 
+/**
+ * Main aplication module
+ * Imports and configures the modules Mongoose, Config, Cars, Auth, Logs
+ */
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://mongo:27017/raidendrive'),
+    MongooseModule.forRoot('mongodb://localhost:27017/raidendrive'),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    CarsModules, 
-    AuthModule, LogsModule,
+    CarsModules,
+    AuthModule,
+    LogsModule,
+    PaymentModule,
   ],
 })
 export class AppModule { }
