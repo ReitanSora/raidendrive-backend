cf2c1cff43611ef698d81879fc419e636510d803
Stiven <stivenpilca@gmail.com>
2025-03-06 19:54:03 -0500

    docs: add inline comments

--- src/types/custom-request.interface.ts
+++ src/types/custom-request.interface.ts
@@ -1,6 +1,14 @@
 import { Request } from 'express';
 
+/**
+ * Custom interface that inherits from the Request class
+ */
 export interface CustomRequest extends Request {
+  /**
+   * User data
+   * Specific attribute user, composed of sub, username and email
+   * @type {{sub: string, username: string, email: string} | undefined}
+   */
   user?: {
     sub: string,
     username: string,
