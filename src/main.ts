486a6c94ecfd3fabeee79ded42e45ce9ca35c9bb
Stiven <stivenpilca@gmail.com>
2025-03-06 19:54:49 -0500

    docs: add inline comments

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+/**
+ * Main function that initialize NestJS aplication
+ * Configure Global Pipes to validate DTO's attributes and ingnoring what is not declared
+ * Enable CORS in aplication
+ */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Global Pipes configuration
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }));
+
+  // Configuration of CORS
   app.enableCors();
+
+  // Port configuration
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
